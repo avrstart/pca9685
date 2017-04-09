3b0f48a16c5cc87606ec47cb9e719aa51bab13e0
Dart <sttn@mail.ru>
2017-04-10 00:26:26 +0300

    added example, checked libs with rpi_b and adafruit pca9685 board

--- pca9685.c
+++ pca9685.c
@@ -1,1 +1,65 @@
-void 
+#include <fcntl.h>   
+#include <stdio.h>   
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <stdint.h> 
+#include "pca9685.h"
+
+static void set_prescaler(int pwm_freq);
+static void i2c_write_reg(uint8_t reg, uint8_t data);
+static void i2c_init(void);
+
+int fp;
+
+static void i2c_write_reg(uint8_t reg, uint8_t data)
+{
+    uint8_t buf[2];
+	
+    buf[0] = reg;
+    buf[1] = data;
+    write(fp, buf, 2);
+}
+
+static void i2c_init(void)
+{
+   char *filename = (char*)"/dev/i2c-1"; 
+  
+   if((fp = open(filename, O_RDWR)) < 0){ }
+   
+   int addr = PCA9685_I2C_ADR;
+   if(ioctl(fp, I2C_SLAVE, addr) < 0){   }   
+}
+
+void set_pwm(int on, int off, int channel)
+{
+   i2c_write_reg((PCA9685_LED0 + 4*channel),     (on & 0xFF));
+   i2c_write_reg((PCA9685_LED0 + 1 + 4*channel), (on >> 8));
+   i2c_write_reg((PCA9685_LED0 + 2 + 4*channel), (off & 0xFF));
+   i2c_write_reg((PCA9685_LED0 + 3 + 4*channel), (off >> 8));
+}
+
+void init_9685(void)
+{
+   i2c_init();                          //platform specific
+   
+   i2c_write_reg(PCA9685_MODE1, 17);    //goto sleep
+   set_prescaler(50);                   //50Hz
+   i2c_write_reg(PCA9685_MODE1, 1);     //run mode
+}
+
+static void set_prescaler(int pwm_freq)
+{
+   uint8_t presc = (25000000 / (4096 * pwm_freq)) - 1;   //presc value
+   i2c_write_reg(PCA9685_PRESC, presc);
+}
+
+void set_servo(int us_value, uint8_t channel)
+{
+   int off_time = (us_value * 4096)/20000;
+   set_pwm(0, off_time, channel);
+}
+
+
+
+
--- pca9685.h
+++ pca9685.h
@@ -1,1 +1,14 @@
-#define
+#ifndef PCA9685_H
+#define PCA9685_H
+
+#define   PCA9685_MODE1       0x00
+#define   PCA9685_MODE2       0x01
+#define   PCA9685_LED0        0x06
+#define   PCA9685_PRESC       0xFE
+#define   PCA9685_I2C_ADR     0x40
+
+void init_9685(void);
+void set_servo(int us_value, uint8_t channel);
+void set_pwm(int on, int off, int channel);
+
+#endif
